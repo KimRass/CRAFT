c006c5bc219f51ee4e55dde3a271f5191036ce10
KimRass <rmx1000@yonsei.ac.kr>
2023-02-27 00:14:27 +0900

    Weakly-supervised; conf score map

--- train_craft/weakly_supervied_learning.py
+++ train_craft/weakly_supervied_learning.py
@@ -1,5 +1,5 @@
-def get_confidence_score_map(gt_length, pred_length):
-    conf_score_map = (
+def get_confidence_score(gt_length, pred_length):
+    conf_score = (
         gt_length - min(gt_length, abs(gt_length - pred_length))
     ) / gt_length
-    return conf_score_map
+    return conf_score
