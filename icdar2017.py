903bfe898f7d2aca68a95131ecb4f0c5baa9225d
KimRass <rmx1000@yonsei.ac.kr>
2023-08-24 22:44:10 +0900

    Generating gts

--- icdar2017.py
+++ icdar2017.py
@@ -1,4 +1,6 @@
 # Source: https://rrc.cvc.uab.es/?ch=8&com=downloads
+import sys
+sys.path.insert(0, "/Users/jongbeomkim/Desktop/workspace/craft")
 
 import torch
 import torch.nn.functional as F
@@ -36,28 +38,24 @@ class CIDAR2017(Dataset):
         if not img_path.exists():
             img_path.with_suffix(".png")
         image = Image.open(img_path).convert("RGB")
-        transform = T.Compose([
-            T.ToTensor(),
-            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
-        image = transform(image)
+        # transform = T.Compose([
+        #     T.ToTensor(),
+        #     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        # ])
+        # image = transform(image)
         
         label_path = self.data_dir/f"""ch8_training_localization_transcription_gt_v2/gt_img_{idx}.txt"""
         with open(label_path, mode="r") as f:
             lines = f.readlines()
-        quads = list()
+        dst_quads = list()
         texts = list()
         for line in lines:
             x1, y1, x2, y2, x3, y3, x4, y4, lang, text = line.strip().split(",")
-            # label.append((x1, y1, x2, y2, x3, y3, x4, y4, text))
-            # quads.append(tuple(map(int, [x1, y1, x2, y2, x3, y3, x4, y4])))
-            quads.append(
-                [[int(x1), int(y1)], [int(x2), int(y2)], [int(x3), int(y3)], [int(x4), int(y4)]]
-            )
+            dst_quads.append([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
             texts.append(text)
         return {
             "image": image,
-            "quadrilaterals": quads,
+            "quadrilaterals": np.array(dst_quads).astype("float32"),
             "texts": texts,
         }
 
@@ -72,15 +70,41 @@ if __name__ == "__main__":
     ds = CIDAR2017(data_dir)
     batch = ds[127]
     image = batch["image"]
-    quads = batch["quadrilaterals"]
+    dst_quads = batch["quadrilaterals"]
     # image.show()
     # vis = draw_quads(image=image, quads=quads)
     # vis.show()
 
-    for src_quad in quads:
-        transformed = _transform_image(image, src_quad)
-        transformed.shape
-
+    gauss = _get_2d_isotropic_gaussian_map()
+    w = h = 200
+    margin = 0.1
+    xmin = w * margin
+    ymin = h * margin
+    xmax = w * (1 - margin)
+    ymax = h * (1 - margin)
+    src_quad = np.array(
+        [[xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax]], dtype="float32"
+    )
+    
+    img = np.array(image)
+    show_image(img)
+    for src_quad in dst_quads:
+        dst_quad = _get_dst_quad(src_quad)
+        M = cv2.getPerspectiveTransform(src=src_quad, dst=dst_quad)
+        w = round(dst_quad[2, 0])
+        h = round(dst_quad[2, 1])
+        output = cv2.warpPerspective(src=img, M=M, dsize=(w, h))
+        show_image(output)
+    
+    # _, h, w = image.shape
+    w, h = image.size
+    gt = np.zeros(shape=(h, w))
+    for dst_quad in dst_quads:
+        M = cv2.getPerspectiveTransform(src=src_quad, dst=dst_quad)
+        output = cv2.warpPerspective(src=gauss, M=M, dsize=(w, h))
+        gt = np.maximum(gt, output)
+    vis_score_map(image=image, score_map=gt)
+    
         model.eval()
         _, h, w = transformed.shape
         with torch.no_grad():
@@ -88,4 +112,4 @@ if __name__ == "__main__":
         z0 = z[0, :, :, 0].detach().cpu().numpy()
         resized_z0 = cv2.resize(z0, dsize=(w, h))
         region_map = _normalize_score_map(resized_z0)
-        Image.fromarray(region_map).show()
+        # Image.fromarray(region_map).show()
--- utils.py
+++ utils.py
@@ -75,31 +75,87 @@ def _get_l2_dist(p1, p2):
     return np.linalg.norm(p1 - p2, ord=2)
 
 
-# def _get_dst_quad(src_quad):
-#     w1 = _get_l2_dist(p1=src_quad[0], p2=src_quad[1])
-#     w2 = _get_l2_dist(p1=src_quad[2], p2=src_quad[3])
-#     h1 = _get_l2_dist(p1=src_quad[1], p2=src_quad[2])
-#     h2 = _get_l2_dist(p1=src_quad[3], p2=src_quad[0])
-#     w = (w1 + w2) // 2
-#     h = (h1 + h2) // 2
-#     dst_quad = np.array(((0, 0), (w, 0), (w, h), (0, h)), dtype="float32")
-#     return dst_quad
-
-
 def _get_dst_quad(src_quad):
-    x1, y1, x2, y2, x3, y3, x4, y4 = sum(src_quad, [])
-    w1 = round(((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5)
-    w2 = round(((x3 - x4) ** 2 + (y3 - y4) ** 2) ** 0.5)
-    h1 = round(((x2 - x3) ** 2 + (y2 - y3) ** 2) ** 0.5)
-    h2 = round(((x4 - x1) ** 2 + (y4 - y1) ** 2) ** 0.5)
+    w1 = _get_l2_dist(p1=src_quad[0], p2=src_quad[1])
+    w2 = _get_l2_dist(p1=src_quad[2], p2=src_quad[3])
+    h1 = _get_l2_dist(p1=src_quad[1], p2=src_quad[2])
+    h2 = _get_l2_dist(p1=src_quad[3], p2=src_quad[0])
     w = (w1 + w2) // 2
     h = (h1 + h2) // 2
-    dst_quad = [[0, 0], [w, 0], [w, h], [0, h]]
+    dst_quad = np.array(((0, 0), (w, 0), (w, h), (0, h)), dtype="float32")
     return dst_quad
 
 
-def _transform_image(image, src_quad):
-    dst_quad = _get_dst_quad(src_quad)
-    new_image = TF.perspective(image, src_quad, dst_quad)
-    new_image = new_image[:, : dst_quad[2][1], : dst_quad[2][0]]
-    return new_image
+# def _get_dst_quad(src_quad):
+#     # src_quad = dst_quad.copy()
+#     x1, y1, x2, y2, x3, y3, x4, y4 = sum(src_quad, [])
+#     w1 = round(((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5)
+#     w2 = round(((x3 - x4) ** 2 + (y3 - y4) ** 2) ** 0.5)
+#     h1 = round(((x2 - x3) ** 2 + (y2 - y3) ** 2) ** 0.5)
+#     h2 = round(((x4 - x1) ** 2 + (y4 - y1) ** 2) ** 0.5)
+#     w = (w1 + w2) // 2
+#     h = (h1 + h2) // 2
+#     dst_quad = [[0, 0], [w, 0], [w, h], [0, h]]
+#     return dst_quad
+
+
+# def _transform_image(image, src_quad):
+#     dst_quad = _get_dst_quad(src_quad)
+#     new_image = TF.perspective(image, src_quad, dst_quad)
+#     new_image = new_image[:, : dst_quad[2][1], : dst_quad[2][0]]
+#     return new_image
+
+
+def _get_2d_isotropic_gaussian_map(w=200, h=200, sigma=0.4):
+    x, y = np.meshgrid(
+        np.linspace(-1, 1, w), np.linspace(-1, 1, h)
+    )
+    d = np.sqrt(x ** 2 + y ** 2)
+    mu = 0
+    gaussian_map = np.exp(
+        -(d - mu) ** 2 / (2 * sigma ** 2)
+    )
+
+    # gaussian_map *= 255
+    # gaussian_map = gaussian_map.astype("uint8")
+    return gaussian_map
+
+
+def _get_gaussian_map_core_rectangle(gaussian_map, margin=0.4):
+    w, h = _get_width_and_height(gaussian_map)
+    xmin = w * margin
+    ymin = h * margin
+    xmax = w * (1 - margin)
+    ymax = h * (1 - margin)
+    return xmin, ymin, xmax, ymax
+
+
+def _to_pil(img, mode="RGB"):
+    if not isinstance(img, Image.Image):
+        image = Image.fromarray(img, mode=mode)
+        return image
+    else:
+        return img
+
+
+def show_image(image):
+    _to_pil(image).show()
+
+
+def show_blended_image(image1, image2, alpha=0.5):
+    image1 = _to_pil(image1)
+    image2 = _to_pil(image2)
+    blended = Image.blend(image1, image2, alpha=alpha)
+    blended.show()
+
+
+def _apply_jet_colormap(img):
+    img_jet = cv2.applyColorMap(src=(255 - img), colormap=cv2.COLORMAP_JET)
+    return img_jet
+
+
+def vis_score_map(image, score_map):
+    score_map *= 255
+    score_map = score_map.astype("uint8")
+    score_map = _apply_jet_colormap(score_map)
+    show_blended_image(image, score_map)
